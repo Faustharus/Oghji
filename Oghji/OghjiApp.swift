5201a9b69d657ff5ed80e774fd31a1b625e81767
Dams920 <50359853+Dams920@users.noreply.github.com>
2023-03-25 14:51:36 +0100

    Initial Commit

---
+++ Oghji/OghjiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OghjiApp.swift
+//  Oghji
+//
+//  Created by Damien Chailloleau on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct OghjiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
